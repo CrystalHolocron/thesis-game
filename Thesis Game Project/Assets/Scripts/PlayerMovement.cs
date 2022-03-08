e9a3e354d4b5b5b9ee3edff049a2cb02a72ffc4d
Ryan Talbot <dtalbot@stedwards.edu>
2022-03-08 15:26:40 -0600

    Implemented jump & fixed player rotation

--- Thesis Game Project/Assets/Scripts/PlayerMovement.cs
+++ Thesis Game Project/Assets/Scripts/PlayerMovement.cs
@@ -6,33 +6,57 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed;
     public float rotationSpeed;
+    public float jumpSpeed;
 
     private CharacterController characterController;
+    private float ySpeed;
+    private float originalStepOffset;
     
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizonatlInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizonatlInput, 0, verticalInput);
-        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * movementSpeed; // Ensures less movement if joypad stick is not fully tilted
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        float magnitude = Mathf.Clamp01(movementDirection.magnitude) * movementSpeed; // Ensures less movement if joystick is not fully tilted
         movementDirection.Normalize(); // Locks movementDirection vector to 1 so diagonal movement isn't faster than ordinal movement
 
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+            if (Input.GetButtonDown("Jump"))
+            {
+                ySpeed = jumpSpeed;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+
         // transform.Translate(movementDirection * magnitude * Time.deltaTime, Space.World); // movement
-        characterController.SimpleMove(movementDirection * magnitude); // movement using the CharacterController
+        Vector3 velocity = movementDirection * magnitude;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime); // movement using the CharacterController
 
         if (movementDirection != Vector3.zero)
         {
             // transform.forward = movementDirection; // instantly snaps character to face movementDirection
 
             Quaternion toRotate = Quaternion.LookRotation(movementDirection, Vector3.up);
+            toRotate *= Quaternion.Euler(0, -90, 0); // Adds a -90/270 degree Y rotation to account for incorrect toRotate angle
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotate, rotationSpeed * Time.deltaTime);
         }
